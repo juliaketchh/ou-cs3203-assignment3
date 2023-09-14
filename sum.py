7aef64c50a09d0ce11092bf40cfdb0b711dd8afa
Julia <juliaketchh@gmail.com>
2023-09-13 21:04:25 -0500

    added products

--- sum.py
+++ sum.py
@@ -5,3 +5,11 @@ def sum(nums):
     return total
 
 print(sum([1,2,3,4]))
+
+def product(nums):
+    total = 1
+    for i in nums:
+        total = total * i
+    return total
+
+print(product([1,2,3]))
